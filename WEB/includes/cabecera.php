756f4c1518bbccd8edc8fe0eaec54a02b294741f
AlainMaristak <alain.basterra@maristak.net>
2024-11-22 11:58:56 +0100

    Estructura cabecera/nabvar corregido

--- WEB/includes/cabecera.php
+++ WEB/includes/cabecera.php
@@ -4,23 +4,14 @@
 
   <ul class="navbar-nav flex-row d-md-none">
     <li class="nav-item text-nowrap">
-      <button class="nav-link px-3 text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSearch" aria-controls="navbarSearch" aria-expanded="false" aria-label="Toggle search">
-          <i class="bi bi-search"></i>
-      </button>
-    </li>
-    <li class="nav-item text-nowrap">
       <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
           <i class="bi bi-list"></i>
       </button>
     </li>
   </ul>
-
-  <div id="navbarSearch" class="navbar-search w-100 collapse">
-    <input class="form-control w-100 rounded-0 border-0" type="text" placeholder="Search" aria-label="Search">
-  </div>
 </header>
 <!-- FIN HEADER/CABECERA -->
 
 <!-- CONTENEDOR PRINCIPAL DE LA PÁGINA -->
-<div class="container-fluid h-100">
-  <div class="row">
+<div class="container-fluid altura-container">
+  <div class="row altura-row">
